9d78c4e34c39dbacbb141d5fb3b817bcf8fa6d3c
Adithya Harsha Babu <ahb437@nyu.edu>
2020-09-06 18:44:31 -0400

    Update chat.js

--- src/chat.js
+++ src/chat.js
@@ -48,7 +48,7 @@ function Chat({messages}) {
             <div className="chat__body">
                 {messages.map((message) => (
 
-<p className= 'chat__message ${message.received && "chat__receiver"'>}
+<p className= 'chat__message ${message.received && "chat__receiver"'>
 <span className="chat__name">
   {message.name}  
 </span>
